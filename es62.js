d64e39425b34932fd054bae31329bec0be3996fb
Dzulfanni <belajarayok866@gmail.com>
2020-09-15 22:03:50 +0700

    first commit

---
+++ es62.js
@@ -0,0 +1,10 @@
+// no2
+const arr = [90, 102, 1231, 3112, 801, 812];
+
+var [a, b, c, d, e, f]= arr;
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
